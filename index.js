d18ceca90adfe42177ec22b2dabf3b52b8f89963
+ <+>
2024-01-09 07:56:09 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your solution in this file!
+variable employee name streetAddress
+
+function updateEmployeeWithKeyAndValue(employee, Object, key, value)
+
+employee Object, a key and a value
+
+return Object value key
+
+destructivelyUpdateEmployeeWithKeyAndValue()
+
+destructivelyDeleteFromEmployeeByKey()
