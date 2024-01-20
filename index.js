6556d3c7edabe3136d4cb10484098f782cbe9915
+ <+>
2024-01-20 14:34:40 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-variable employee name streetAddress
+let employee = {
+    name: "{John Doe}",
+    streetAddress: `{123 Main Street}`
+  };
 
-function updateEmployeeWithKeyAndValue(employee, Object, key, value)
-
-employee Object, a key and a value
-
-return Object value key
-
-destructivelyUpdateEmployeeWithKeyAndValue()
-
-destructivelyDeleteFromEmployeeByKey()
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
