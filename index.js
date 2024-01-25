9d046ccf7665f19b1a03bbbc547abba149104434
+ <+>
2024-01-25 15:52:41 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 let employee = {
-    name: "{John Doe}",
-    streetAddress: `{123 Main Street}`
-  };
+    name: "John Doe",
+    streetAddress: "123 Main St"
+};
 
-  function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return { ...employee, [key]: value };
-  }
-  
-  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const copyemployee = {...employee}
+
+    copyemployee[key] = value;
+
+    return copyemployee;
+    
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    
     employee[key] = value;
+
     return employee;
-  }
-  
-  function deleteFromEmployeeByKey(employee, key) {
-    let newEmployee = { ...employee };
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {employee};
     delete newEmployee[key];
     return newEmployee;
-  }
-  
-  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key];
     return employee;
-  }
-  
+}
+
+
+
+
+
+
+
+
+
